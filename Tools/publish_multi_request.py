64856a3b37d17546b43fb607531335ea6795d145
superjeepboy <superjeepboy@hotmail.com>
2025-09-06 03:18:44 -0700

    Publish link fixing (again)

--- Tools/publish_multi_request.py
+++ Tools/publish_multi_request.py
@@ -15,7 +15,7 @@ RELEASE_DIR = "release"
 # CONFIGURATION PARAMETERS
 # Forks should change these to publish to their own infrastructure.
 #
-ROBUST_CDN_URL = "https://cdn.boxstation.me/"
+ROBUST_CDN_URL = "http://cdn.boxstation.me/"
 FORK_ID = "boxstation"
 
 def main():
