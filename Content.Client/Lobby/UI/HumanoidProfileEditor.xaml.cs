886a32a50db04dd8b4a30a0e24bceefe335b399b
Milon <milonpl.git@proton.me>
2026-01-25 16:12:38 +0100

    more traits improvements (#5285)
    * wowie
    
    * whoopsie
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * WHY DID I DO THAT
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- Content.Client/Lobby/UI/HumanoidProfileEditor.xaml.cs
+++ Content.Client/Lobby/UI/HumanoidProfileEditor.xaml.cs
@@ -526,6 +526,7 @@ namespace Content.Client.Lobby.UI
             }
 
             Traits.SetSelectedTraits(selectedTraits);
+            Traits.UpdateConditions(Profile.Species);
         }
         // End DeltaV - Traits Integration
 
