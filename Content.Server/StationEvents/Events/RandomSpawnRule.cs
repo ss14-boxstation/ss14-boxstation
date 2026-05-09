72b52f93d044f9ecc440e826bf4636bcf3f5a8ed
BlueMoon059 <205945990+BlueMoon059@users.noreply.github.com>
2026-05-09 01:10:09 +0100

    first pass

--- Content.Server/StationEvents/Events/RandomSpawnRule.cs
+++ Content.Server/StationEvents/Events/RandomSpawnRule.cs
@@ -1,11 +1,20 @@
 using Content.Server.GameTicking.Rules.Components;
 using Content.Server.StationEvents.Components;
 using Content.Shared.GameTicking.Components;
-
+//Box - Moffstation - Start - Syndicate dead drop
+using Content.Server.Radio.EntitySystems;
+using Content.Server.Pinpointer;
+using Robust.Shared.Utility;
+// Box - Moffstation - End
 namespace Content.Server.StationEvents.Events;
 
 public sealed class RandomSpawnRule : StationEventSystem<RandomSpawnRuleComponent>
 {
+    //Box - Moffstation - Start - Syndicate dead drop
+    [Dependency] private readonly NavMapSystem _navMap = default!;
+    [Dependency] private readonly RadioSystem _radio = default!;
+    //Box - Moffstation - End
+
     protected override void Started(EntityUid uid, RandomSpawnRuleComponent comp, GameRuleComponent gameRule, GameRuleStartedEvent args)
     {
         base.Started(uid, comp, gameRule, args);
@@ -13,7 +22,16 @@ public sealed class RandomSpawnRule : StationEventSystem<RandomSpawnRuleComponen
         if (TryFindRandomTile(out _, out _, out _, out var coords))
         {
             Sawmill.Info($"Spawning {comp.Prototype} at {coords}");
-            Spawn(comp.Prototype, coords);
+            //Spawn(comp.Prototype, coords); Box - Moffstation - Comments out line for deaddrop PR
+            //Box - Moffstation - Syndicate dead drop
+            var ent = Spawn(comp.Prototype, coords);
+
+            if (comp.RadioMessage is {} radioMessage)
+            {
+                var message = Loc.GetString(radioMessage.Message, ("location", FormattedMessage.RemoveMarkupOrThrow(_navMap.GetNearestBeaconString(ent))));
+                _radio.SendRadioMessage(ent, message, radioMessage.Channel, ent);
+            }
+            //Box - Moffstation - End
         }
     }
 }
