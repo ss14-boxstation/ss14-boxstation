4035fad7607f8acfa914f0d050ff1f631409469c
Rami <ramialanbagy@gmail.com>
2026-05-23 13:29:19 -0700

    Fix Barber Scissors not playing audio (#43922)
    * Fix Barber Scissors not playing audio
    
    * Add code comment
    
    * Requested changes
    
    * More requested changes thanks Warock
    
    * Requested changes

--- Content.Client/MagicMirror/MagicMirrorBoundUserInterface.cs
+++ Content.Client/MagicMirror/MagicMirrorBoundUserInterface.cs
@@ -24,7 +24,7 @@ public sealed class MagicMirrorBoundUserInterface : BoundUserInterface
 
         _markingsModel.MarkingsChanged += (_, _) =>
         {
-            SendMessage(new MagicMirrorSelectMessage(_markingsModel.Markings));
+            SendPredictedMessage(new MagicMirrorSelectMessage(_markingsModel.Markings));
         };
     }
 
