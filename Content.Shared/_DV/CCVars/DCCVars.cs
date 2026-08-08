1b126c32d882d56ff6e35148ef799bf9c357fa8b
Dizzy <kido@kido.moe>
2026-08-08 12:28:16 -0400

    revert cvar changes
    i wound up being able to gut the points/count system entirely so these changes are unnecessary

--- Content.Shared/_DV/CCVars/DCCVars.cs
+++ Content.Shared/_DV/CCVars/DCCVars.cs
@@ -14,14 +14,14 @@ public sealed partial class DCCVars
     /// Maximum number of traits that can be selected globally.
     /// </summary>
     public static readonly CVarDef<int> MaxTraitCount =
-        CVarDef.Create("traits.max_count", 1000, CVar.SERVER | CVar.REPLICATED); // Box Change: Make this an arbitrarily high value; change it later if we decide we want to go the "positive traits with point balance" route
+        CVarDef.Create("traits.max_count", 10, CVar.SERVER | CVar.REPLICATED);
 
     /// <summary>
     /// Maximum trait points available to spend.
     /// Traits with positive cost consume points, negative cost traits grant points.
     /// </summary>
     public static readonly CVarDef<int> MaxTraitPoints =
-        CVarDef.Create("traits.max_points", 1000, CVar.SERVER | CVar.REPLICATED); // Box Change: Make this an arbitrarily high value; change it later if we decide we want to go the "positive traits with point balance" route
+        CVarDef.Create("traits.max_points", 15, CVar.SERVER | CVar.REPLICATED);
 
     /// <summary>
     /// Whether to skip showing the disabled traits popup when spawning.
