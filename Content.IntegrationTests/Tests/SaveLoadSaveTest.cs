37e8c22039bb7b5549c483072674f3692068643d
Bones <150093917+CollectionOfBones128@users.noreply.github.com>
2026-08-11 04:10:55 +0100

    Hardfork maps (#252)
    * remove map modifications
    
    if we need to modify the maps, we can just add to them soon
    
    * hardfork wizden
    
    * hardfork harmony maps
    
    * restore atlas
    
    * hardfork atlas
    
    * update pathways
    
    * default pool and tests
    
    * adjust job numbers and player counts
    
    readd supply assistant and minor map tweaks
    
    * comment out atlas supply assistant
    
    no spawner has been mapped yet as its pending a major rework
    
    * revert elkridge code
    
    omega's map file accidentally got put in as elkridge
    
    * testfail fixes

--- Content.IntegrationTests/Tests/SaveLoadSaveTest.cs
+++ Content.IntegrationTests/Tests/SaveLoadSaveTest.cs
@@ -88,13 +88,13 @@ namespace Content.IntegrationTests.Tests
             await pair.CleanReturnAsync();
         }
 
-        private const string TestMap = "Maps/bagel.yml";
+        private const string TestMap = "Maps/_Box/elkridge.yml"; // Box Change - bagel > Elkridge - Hardfork Maps
 
         /// <summary>
         ///     Loads the default map, runs it for 5 ticks, then assert that it did not change.
         /// </summary>
         [Test]
-        public async Task LoadSaveTicksSaveBagel()
+        public async Task LoadSaveTicksSaveElkridge() // Box Change - bagel > Elkridge - Hardfork Maps
         {
             await using var pair = await PoolManager.GetServerClient();
             var server = pair.Server;
@@ -110,7 +110,7 @@ namespace Content.IntegrationTests.Tests
             var cfg = server.ResolveDependency<IConfigurationManager>();
             Assert.That(cfg.GetCVar(CCVars.GridFill), Is.False);
 
-            // Load bagel.yml as uninitialized map, and save it to ensure it's up to date.
+            // Load elkridge.yml as uninitialized map, and save it to ensure it's up to date. // Box Change - bagel > Elkridge - Hardfork Maps
             server.Post(() =>
             {
                 var path = new ResPath(TestMap);
@@ -177,11 +177,11 @@ namespace Content.IntegrationTests.Tests
         /// <remarks>
         ///     Should ensure that entities do not perform randomization prior to initialization and should prevents
         ///     bugs like the one discussed in github.com/space-wizards/RobustToolbox/issues/3870. This test is somewhat
-        ///     similar to <see cref="LoadSaveTicksSaveBagel"/> and <see cref="SaveLoadSave"/>, but neither of these
+        ///     similar to <see cref="LoadSaveTicksSaveElkridge"/> and <see cref="SaveLoadSave"/>, but neither of these
         ///     caught the mentioned bug.
         /// </remarks>
         [Test]
-        public async Task LoadTickLoadBagel()
+        public async Task LoadTickLoadElkridge() // Box Change - bagel > Elkridge - Hardfork Maps
         {
             await using var pair = await PoolManager.GetServerClient();
             var server = pair.Server;
